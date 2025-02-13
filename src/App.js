3b2608b104868a70daaacd3441cc4e29824cd752
Pedro Alejandro Avila <alejox31@gmail.com>
2025-02-13 10:30:36 -0500

    adding git ignore

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to React</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
